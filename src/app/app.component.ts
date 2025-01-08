b0ca9e842c1ebb7e809deb9eaa83b952122b1102
Kunal Ranjan <kunalranjanblr@gmail.com>
2025-01-08 18:33:15 +0530

    implement RestService for HTTP requests and update app component to utilize it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,119 +1,30 @@
-import { Component, computed, signal, ViewEncapsulation } from '@angular/core';
+import { Component, computed, inject, OnInit, signal, ViewEncapsulation } from '@angular/core';
 import { SampleComponent } from './sample/sample.component';
 import { DemoComponent } from './demo/demo.component';
+import { RestService } from './rest.service';
+
+export interface PostModel {
+ userId: number;
+ id: number;
+ title: string;
+ body: string;
+}
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [SampleComponent,DemoComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  encapsulation: ViewEncapsulation.Emulated,
+ selector: 'app-root',
+ standalone: true,
+ imports: [],
+ templateUrl: './app.component.html',
+ styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ restService = inject(RestService);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   cssClass = computed(() =>
-//    this.counter() >= 10 ? ['text-danger', 'fw-bold'] : 'text-primary'
-//  );
-
-
-//   counter = signal(1);
-  
-//   increment() {
-//     this.counter.update((v)=>v+1);
-//   }
-
-//   decrement() {
-//     this.counter.update((v)=>v-1);
-//   }
-
-
-
-
-//   High = false;
-//   Toggle() {
-//   this.High = !this.High;
-// }
-//   message = 'Hello, world!';
-//   handleMessage(m: string) {
-//    console.log('message received - ', m);
-//  }
-
-// //   handleClick(ar: MouseEvent) {
-// //    console.log('Button is clicked', ar);
-//   //  }
-  
-// //   handleInput(e: Event) {
-// //    const ie = (e as InputEvent).target as HTMLInputElement;
-// //    console.log('typed', ie.value);
-// //  }
-//   username = '';
-//  password = '';
-
-
-//  onLogin() {
-//    const obj = {
-//      username: this.username,
-//      password: this.password,
-//    };
-
-
-//    this.username = '';
-//    this.password = '';
-
-
-//    console.log('Login submitted', obj);
-//  }
-
-
+ ngOnInit(): void {
+   this.restService.get<PostModel[]>('posts').subscribe({
+     next: (p) => console.log('post', p),
+   });
+ }
 }
-function Signal(arg0: number) {
-  throw new Error('Function not implemented.');
-}
-
